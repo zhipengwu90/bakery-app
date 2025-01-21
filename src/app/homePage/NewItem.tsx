e2d4eb84128a1f3aa7d303ce8c9974e46ba04136
zhipengwu90 <zhipengwu90@gmail.com>
2025-01-20 21:39:15 -0800

    update new product

---
+++ src/app/homePage/NewItem.tsx
@@ -0,0 +1,67 @@
+"use client";
+import React, { useState } from "react";
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
+import Modal from "@mui/material/Modal";
+import Image from "next/image";
+import { useMediaQuery } from "@mui/material";
+import NewYearCake from "../../../public/newItems/newYearCake.jpg";
+import newYearBg from "../../../public/newItems/newYearBg.png";
+import snake from "../../../public/newItems/snake.png";
+
+type Props = {};
+
+const NewItem = (props: Props) => {
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
+
+  return (
+    <div className="px-32 lg:px-4 my-3">
+      <div className="flex flex-row items-center justify-center w-full my-8">
+        <div className="flex-grow border-t border-red-500"></div>
+        <div className="px-4 text-center text-2xl font-bold text-red-500">
+          Taste What’s New
+        </div>
+        <div className="flex-grow border-t border-red-500"></div>
+      </div>
+      <div className="grid grid-cols-2 gap-4 md:grid-cols-1 ">
+        <Image
+          src={NewYearCake}
+          alt="newYearCake"
+          className="rounded-lg shadow-lg h-full"
+        />
+        <div
+          className="relative 
+          sm:h-[75vh]
+        
+
+        border-2 border-red-400 rounded-lg p-6  shadow-lg"
+        >
+          <Image
+            src={newYearBg}
+            alt="newYearBg"
+            className="absolute inset-0 w-full h-full object-cover rounded-lg shadow-lg"
+          />
+          <div className="relative z-10  text-red-800">
+            <p className="font-bold text-md md:text-md  ">
+              Celebrate the Lunar New Year with Brazen Poppy’s limited-edition
+              6-inch cakes! Available in Sea Salt Oreo and Seasonal Fruit
+              flavors for $45, these cakes are perfect for adding sweetness to
+              your family gatherings or reunions with friends. Special delivery
+              is available on the 28th and 29th of the Lunar New Year.
+            </p>
+
+            <p className="font-bold ">
+              The cake features the Chinese characters “新年快乐” (Happy New
+              Year) and a decorative red square with the character “福”
+              (Fortune), symbolizing good luck and prosperity for the year
+              ahead. Order now and share the joy of a sweet Lunar New Year!
+            </p>
+            {/* <Image src={snake} alt="snake" className="w-1/2" /> */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NewItem;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Hero from "./homePage/Hero";
 import FoodPhoto from "./homePage/FoodPhoto";
+import NewItem from "./homePage/NewItem";
 
 export default function Home() {
   return (
     <div>
       <Hero />
+      <NewItem />
       <FoodPhoto />
     </div>
   );
