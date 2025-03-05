d7f82832eb3e4edd6f0ba0ac666aa236c49cf862
zhipengwu90 <zhipengwu90@gmail.com>
2025-03-04 21:16:56 -0800

    update menu

--- src/app/menu/menuPart/MenuContent.tsx
+++ src/app/menu/menuPart/MenuContent.tsx
@@ -1,16 +1,21 @@
 "use client";
 import React, { useEffect, useRef, useState } from "react";
 import Image from "next/image";
-// import menu from "../../../../public/menu/menu.jpeg";
 import menu2 from "../../../../public/menu/menu2.jpg";
 import menu from "../../../../public/menu/menu1.jpg";
-import emblaCarousel from "embla-carousel";
-
 import breakfast from "../../../../public/foodPhotos/breakfast.jpeg";
+import emblaCarousel from "embla-carousel";
+import Modal from "@mui/material/Modal";
+import Box from "@mui/material/Box";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
+
 type Props = {};
 
 const MenuContent = (props: Props) => {
-    const [selectedIndex, setSelectedIndex] = useState(0);
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
 
   const emblaRef = useRef(null);
   useEffect(() => {
@@ -19,6 +24,16 @@ const MenuContent = (props: Props) => {
       return () => embla.destroy();
     }
   }, []);
+
+  const openModal = (imageSrc: string) => {
+    setSelectedImage(imageSrc);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setSelectedImage(null);
+  };
 
   return (
     <div className="px-32 lg:px-4 my-3">
@@ -35,55 +50,26 @@ const MenuContent = (props: Props) => {
         to Order, Hamburgers, Gluten Free Options
       </p>
 
-      {/* <div className="embla" ref={emblaRef}>
-        <div className="embla__container">
-          <div className="embla__slide">
-            <div className="embla__slide__number">
-              <Image
-                src={menu}
-                alt="menu"
-                width={100}
-                height={100}
-                layout="responsive"
-                className="shadow-lg max-h-[70vh] "
-              />
-            </div>
-          </div>
-          <div className="embla__slide">
-            <div className="embla__slide__number">
-           
-              <Image
-                src={menu2}
-                alt="menu2"
-                objectFit="contained"
-                layout="responsive"
-                width={100}
-                height={100}
-                className="shadow-lg max-h-[70vh] "
-              />
-            </div>
-          </div>
-        </div>
-      </div> */}
-
       <section className="embla">
         <div className="embla__viewport" ref={emblaRef}>
           <div className="embla__container">
-            <div className="embla__slide">
+            <div className="embla__slide" onClick={() => openModal(menu2.src)}>
               <div className="embla__slide__number">
                 <Image
                   src={menu2}
                   alt="menu"
                   layout="responsive"
-                    objectFit="contain"
-                  
+                  objectFit="contain"
                   width={100}
                   height={100}
-                  className=" shadow-lg max-h-[70vh] "
+                  className="shadow-lg max-h-[70vh] cursor-pointer"
                 />
               </div>
             </div>
-            <div className="embla__slide flex justify-center items-center ">
+            <div
+              className="embla__slide flex justify-center items-center"
+              onClick={() => openModal(menu.src)}
+            >
               <div className="embla__slide__number flex-1">
                 <Image
                   src={menu}
@@ -92,11 +78,14 @@ const MenuContent = (props: Props) => {
                   width={100}
                   height={100}
                   objectFit="contain"
-                  className=" shadow-lg max-h-[70vh]"
+                  className="shadow-lg max-h-[70vh] cursor-pointer"
                 />
               </div>
             </div>
-            <div className="embla__slide">
+            <div
+              className="embla__slide"
+              onClick={() => openModal(breakfast.src)}
+            >
               <div className="embla__slide__number">
                 <Image
                   src={breakfast}
@@ -105,54 +94,65 @@ const MenuContent = (props: Props) => {
                   objectFit="contain"
                   width={100}
                   height={100}
-                  className=" shadow-lg max-h-[70vh] "
+                  className="shadow-lg max-h-[70vh] cursor-pointer"
                 />
               </div>
             </div>
           </div>
         </div>
-        {/* 
-      <div className="embla__controls">
-        <div className="embla__buttons">
-          <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
-          <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
-        </div>
-
-        <div className="embla__dots">
-          {scrollSnaps.map((_, index) => (
-            <DotButton
-              key={index}
-              onClick={() => onDotButtonClick(index)}
-              className={'embla__dot'.concat(
-                index === selectedIndex ? ' embla__dot--selected' : ''
-              )}
-            />
-          ))}
-        </div>
-      </div> */}
       </section>
 
-      {/* <div className="flex flex-row items-center justify-center w-full">
-        <Image
-          src={menu}
-          alt="menu"
-          layout="responsive"
-          width={100}
-          height={100}
-          className=" shadow-lg max-h-[70vh] object-cover"
-        />
-      </div>
-
-      <div className="flex flex-row items-center justify-center w-full">
-        <Image
-          src={menu2}
-          alt="menu"
-          layout="responsive"
-          width={100}
-          height={100}
-          className=" shadow-lg max-h-[70vh] object-cover"
-        />
-      </div> */}
+      <Modal
+        open={isModalOpen}
+        onClose={closeModal}
+        aria-labelledby="modal-title"
+        aria-describedby="modal-description"
+      >
+        <Box
+          sx={{
+            position: "absolute" as "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            width: { xs: "90%", sm: "80%", md: "70%", lg: "60%", xl: "50%" },
+            maxHeight: "90vh",
+            bgcolor: "background.paper",
+            boxShadow: 24,
+            p: 1,
+            outline: "none",
+            overflow: "auto",
+          }}
+        >
+          <IconButton
+            aria-label="close"
+            onClick={closeModal}
+            sx={{
+              position: "absolute",
+              right: 8,
+              top: 8,
+              color: (theme) => theme.palette.grey[900],
+            }}
+          >
+            <div className="bg-white opacity-70 rounded-full p-1">
+              <CloseIcon  
+                fontSize="large"
+        
+              />
+            </div>
+          </IconButton>
+          {selectedImage && (
+            <Image
+              src={selectedImage}
+              alt="Selected"
+              layout="responsive"
+              width={800}
+              height={600}
+              objectFit="contain"
+              className="rounded-lg"
+            />
+          )}
+        </Box>
+      </Modal>
     </div>
   );
 };
